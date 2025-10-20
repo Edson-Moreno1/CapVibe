cd9adf9f7b8b30b8c3c1823ad902765752b3a037
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-10-20 17:22:21 +0000

    Se agrega el lazy Loading

--- ecommerce-app/src/app/app.routes.ts
+++ ecommerce-app/src/app/app.routes.ts
@@ -1,16 +1,31 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './Pages/product-list/product-list.component';
-import { CartComponent } from './Pages/cart/cart.component';
-import { CheckoutComponent } from './Pages/checkout/checkout.component';
-import { ConfirmationComponent } from './Pages/confirmation/confirmation.component';
-import { HomeComponent } from './Pages/home/home.component';
+import {Routes} from '@angular/router';
+import  {HomeComponent} from './Pages/home/home.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }, // Página principal
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'confirmation', component: ConfirmationComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+export const routes:Routes=[
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'products',
+    loadComponent:()=> import ('./Pages/product-list/product-list.component').then(m=>m.ProductListComponent)
+  },
+  {
+    path:'products/:id',
+    loadComponent:()=> import('./Pages/product-detail/product.detail.component').then(m=>m.ProductDetailComponent)
+  },
+  {
+    path:'cart',
+    loadComponent:()=> import ('./Pages/cart/cart.component').then(m=>m.CartComponent)
+  },
+  //Asumiendo que tendrás estas rutas en el futuro para el login/registro
+  //{path:'login',loadComponent:()=>import('./pages/login/login.component').then(m=>m.LoginComponent)},
+  //{path:'register',loadComponent:()=>import('./pages/register/register.component').then(m=>m.RegisterComponent)},
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+  }
+
 ];
